0243269e80fb8056779c80dd4bbce8306471482d
Deussaxum <147807491+Deussaxum@users.noreply.github.com>
2023-11-21 15:10:04 +0100

    change

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 from feature_01 import return_even
 
-original_list [i for i in range(10)]
 
-even_list = return_even(original_list)
 
 
 
@@ -11,4 +9,3 @@ st.write("Horray we connected everything")
 
 st.write("Hello_02")
 
-st.write(even_list)
